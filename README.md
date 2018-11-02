f453cbad89e0a0c6f5d090f3fbba5b6a5e0adc14
Michael Aaron Murphy <mmstickman@gmail.com>
2018-11-02 11:30:01 -0600

    Add an example

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # os-detect
 
 Rust crate for detecting the existence of an OS from an unmounted device, or path.
+
+```rust
+extern crate os_detect;
+
+use os_detect::detect_os_from_device;
+use std::path::Path;
+
+pub fn main() {
+    let device_path = &Path::new("/dev/sda3");
+    let fs = "ext4";
+    if let Some(os) = detect_os_from_device(device_path, fs) {
+        println!("{:#?}", os);
+    }
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,19 @@
 //! Provides the means for for detecting the existence of an OS from an unmounted device, or path.
+//!
+//! ```rust,no_run
+//! extern crate os_detect;
+//! 
+//! use os_detect::detect_os_from_device;
+//! use std::path::Path;
+//! 
+//! pub fn main() {
+//!     let device_path = &Path::new("/dev/sda3");
+//!     let fs = "ext4";
+//!     if let Some(os) = detect_os_from_device(device_path, fs) {
+//!         println!("{:#?}", os);
+//!     }
+//! }
+//! ```
 
 #[macro_use]
 extern crate log;
